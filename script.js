bcef8795fa6b473bee46df9e7e5648b0e56686ed
Matthias Vetter <matze2948@gmail.com>
2023-11-14 13:03:24 +0100

    + function calculateSameCostTime()

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function calculate (cost, mult, duration, worker) {
     return costPerHour;
 }
 
+function calculateSameCostTime (result1, result2, cost) { // res1,2 is cost/hour
+    newRes1 = cost/result1;
+    newRes2 = cost/result2;
+    document.querySelector("#result1SameCostTime").innerHTML = "Time: " +newRes1.toFixed(0)+ " (hours for " +cost.toFixed(0)+" gems)" ;
+    document.querySelector("#result2SameCostTime").innerHTML = "Time: " +newRes2.toFixed(0)+ " (hours per " +cost.toFixed(0)+" gems)";
+}
+
 function calcResult () {
     let cost1 = document.querySelector("#cost1").value;
     let multiplicator1 = document.querySelector("#multiplicator1").value;
@@ -25,9 +32,11 @@ function calcResult () {
     result1 = calculate(cost1, multiplicator1, duration1, workers1);
     result2 = calculate(cost2, multiplicator2, duration2, workers2);
  
-    document.getElementById("result1").innerHTML = "Cost: " + result1 + " (Gems per hour)";
-    document.getElementById("result2").innerHTML = "Cost: " + result2 + " (Gems per hour)";
+    document.getElementById("result1").innerHTML = "Cost: " + result1.toFixed(2) + " (Gems per hour)";
+    document.getElementById("result2").innerHTML = "Cost: " + result2.toFixed(2) + " (Gems per hour)";
     
+    calculateSameCostTime(result1, result2, Math.max(cost1, cost2));
+
     if (isNaN(result1) && isNaN(result2)) {
         document.getElementById("result3").innerHTML = "=> Please enter something" ;
         return;
